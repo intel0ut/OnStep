f16fef891f9b60da4b36cd65b8cf07cbdbf08b0c
Guilherme Venere <venere@gmail.com>
2020-10-20 08:35:31 -0700

    Remove #error message to change HardwareSerial.cpp as this is fixed elsewhere

--- src/pinmaps/Validate.InsteinESP1.h
+++ src/pinmaps/Validate.InsteinESP1.h
@@ -11,7 +11,6 @@
     #if PINMAP == InsteinESP1
       #undef Axis4_EN
       #define Axis4_EN -1
-      #error "Make sure you made the changes to HardwareSerial pins RX1 and TX1 as descrived in Config.InsteinESP1.h. Comment this line after you're done"
     #endif
 #endif
 
